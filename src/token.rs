be410ef8db74377be30be70dde43ec55f753c369
kresnawan <sanjayakresnawan@gmail.com>
2026-03-01 23:37:48 +0700

    Refactor, and major bug fixes
    Pack the utilities into one encapsulated struct for convinience, make the code a lot cleaner, and fix the StartCaret postfix error

--- src/token.rs
+++ src/token.rs
@@ -1,4 +1,4 @@
-use std::fmt::Debug;
+use std::fmt::{Debug, Display};
 
 #[derive(Debug, Clone, Copy)]
 pub enum Token {
@@ -15,6 +15,19 @@ pub enum OperatorType {
     Subtract,
     Caret,
     StartCaret,
+}
+
+impl Display for OperatorType {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            Self::Multiply => write!(f, "*"),
+            Self::Divide => write!(f, "/"),
+            Self::Add => write!(f, "+"),
+            Self::Subtract => write!(f, "-"),
+            Self::Caret => write!(f, "^"),
+            _ => write!(f, ""),
+        }
+    }
 }
 
 #[derive(Debug, Clone, Copy)]
